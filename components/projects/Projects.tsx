0fd4410c4be97f9a1cea8393a4f203eca63cd9e2
AdamZed <adamzanon@hotmail.com>
2022-01-20 20:23:30 -0500

    project skeleton

---
+++ components/projects/Projects.tsx
@@ -0,0 +1,14 @@
+interface Project {
+  name: string;
+  desc: string;
+  stack: string[];
+  repo?: string;
+  link?: string;
+  date?: string;
+}
+
+const projectList: Project[] = [];
+
+export default function Projects() {
+  return <></>;
+}
